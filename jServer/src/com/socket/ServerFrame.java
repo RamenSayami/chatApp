f2dd5303d4b436cf0aa78cdc350ccb7e4880c794
Ramen Sayami <ramen.sayami@gmail.com>
2017-02-27 12:21:52 +0545

    initial commit

---
+++ jServer/src/com/socket/ServerFrame.java
@@ -0,0 +1,146 @@
+package com.socket;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+
+public class ServerFrame extends javax.swing.JFrame {
+
+    public SocketServer server;
+    public Thread serverThread;
+    public String filePath = "D:/Data.xml";
+    public JFileChooser fileChooser;
+    
+    public ServerFrame() {
+        initComponents();     
+        FileTextPath.setEditable(false);
+        FileTextPath.setBackground(Color.WHITE);
+        
+        fileChooser = new JFileChooser();
+        jTextArea1.setEditable(false);
+    }
+    
+    public boolean isWin32(){
+        return System.getProperty("os.name").startsWith("Windows");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        StartServerButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        FileTextPath = new javax.swing.JTextField();
+        browseButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("jServer");
+
+        StartServerButton.setText("Start Server");
+        StartServerButton.setEnabled(false);
+        StartServerButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StartServerButtonActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel3.setText("Database File : ");
+
+        browseButton.setText("Browse...");
+        browseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                browseButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(FileTextPath, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(browseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(StartServerButton)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(FileTextPath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(browseButton)
+                    .addComponent(StartServerButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void StartServerButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StartServerButtonActionPerformed
+        server = new SocketServer(this);
+        StartServerButton.setEnabled(false); browseButton.setEnabled(false);
+    }//GEN-LAST:event_StartServerButtonActionPerformed
+
+    public void RetryStart(int port){
+        if(server != null){ server.stop(); }
+        server = new SocketServer(this, port);
+    }
+    
+    private void browseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_browseButtonActionPerformed
+        fileChooser.showDialog(this, "Select");
+        File file = fileChooser.getSelectedFile();
+        
+        if(file != null){
+            filePath = file.getPath();
+            if(this.isWin32()){ filePath = filePath.replace("\\", "/"); }
+            FileTextPath.setText(filePath);
+            StartServerButton.setEnabled(true);
+        }
+    }//GEN-LAST:event_browseButtonActionPerformed
+
+    public static void main(String args[]) {
+
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception ex){
+            System.out.println("Look & Feel Exception");
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ServerFrame().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField FileTextPath;
+    private javax.swing.JButton StartServerButton;
+    private javax.swing.JButton browseButton;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+}
